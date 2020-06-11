d8a2e3e0610b1a355d23f9843dd6e3ace4c0c0e5
gurongrong0329 <648186030@qq.com>
2020-06-12 01:03:16 +0800

    xxxxx

--- case/test_login.py
+++ case/test_login.py
@@ -3,16 +3,16 @@ from tools.read_json import *
 from parameterized import parameterized
 from module.login import *
 
-def get_data():
-    ret = ReadJson('login.json').read_json()
-    arr = []
-    for k, v in ret.items():
-        arr.append(v)
-    return [tuple(arr)]
+# def get_data():
+#     ret = ReadJson('login.json').read_json()
+#     arr = []
+#     for k, v in ret.items():
+#         arr.append(v)
+#     return [tuple(arr)]
 
 class TestLogin(unittest.TestCase):
 
-    @parameterized.expand(get_data())
+    @parameterized.expand(ReadJson('login.json').get_data())
     def testLogin(self,url,account,password,status):
         ret=Login(url).get_login(account,password)
         self.assertEqual(ret['status'],status)
--- tools/read_json.py
+++ tools/read_json.py
@@ -3,18 +3,44 @@ import json
 
 class ReadJson(object):
 
-    def __init__(self,filename):
-        self.filepath='../data/'+filename
+    def __init__(self, filename):
+        self.filepath = '../data/' + filename
 
     def read_json(self):
-        with open(self.filepath,'r',encoding='utf-8') as f:
+        with open(self.filepath, 'r', encoding='utf-8') as f:
             return json.load(f)
 
-if __name__ == '__main__':
-    ret=ReadJson('login.json').read_json()
-    arr = []
-    for k,v in ret.items():
-        arr.append(v)
+    def get_data(self):
+        ret = self.read_json()
+        arr1 = []
+        arr2 = []
+        arr3 = []
+        for v in ret.values():
+            arr1.append(v)
 
+        for i in arr1:
+            for k, v in i.items():
+                arr2.append(v)
+            arr2 = tuple(arr2)
+            arr3.append(arr2)
+            arr2 = []
+        return arr3
+
+
+if __name__ == '__main__':
+    ret = ReadJson('login.json').read_json()
+    arr1 = []
+    arr2 = []
+    arr3 = []
+    for v in ret.values():
+        arr1.append(v)
+
+    for i in arr1:
+        for k, v in i.items():
+            arr2.append(v)
+        arr2 = tuple(arr2)
+        arr3.append(arr2)
+        arr2 = []
+    print(arr3)
     # arr.append((ret.get('url'),ret.get('account'),ret.get('password'),ret.get('status')))
-    print([tuple(arr)])
+    # print([tuple(arr)])
